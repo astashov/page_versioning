2ca2185253beecf76b6faab2a891a4d006d9ce1d
Anton Astashov <anton@localhost.(none)>
2009-02-19 16:36:33 +0700

    Changed default of published revision from <next> to current number

--- lib/page_versioning/common_helpers.rb
+++ lib/page_versioning/common_helpers.rb
@@ -10,7 +10,7 @@ module PageVersioning::CommonHelpers
   
   def insert_select_published_revision_tag(element, title)
     # Default is 0 ('<next>')
-    options_for_published_revision = options_for_select(element.revisions.map {|r| [r.number, r.number] } + [['<next>', 0]], 0)
+    options_for_published_revision = options_for_select(element.revisions.map {|r| [r.number, r.number] } + [['<next>', 0]], model.published_revision_number)
     select_tag "#{title}[published_revision_number]", options_for_published_revision, :id => "#{title}_published_revision_number"
   end
   
